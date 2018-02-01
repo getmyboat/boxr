dd8e7bd6148c375dcf8f4267e61582819f9dfd63
Alex Hubers <ahubers15@cornellcollege.edu>
2018-02-01 17:43:37 -0600

    Make test wishlist in my Development Driven Testing

--- boxr/tests.py
+++ boxr/tests.py
@@ -9,3 +9,35 @@ def test_coercing_a_string():
 
 def test_coercing_a_date():
     assert coerce_date(date(2017, 1, 1)) == '2017-01-01'
+
+
+def test_get_calls_session_get():
+    pass
+
+
+def test_getting_latest_makes_request():
+    pass
+
+
+def test_getting_historical_makes_request():
+    pass
+
+
+def test_getting_currencies_makes_request():
+    pass
+
+
+def test_getting_time_series_makes_request():
+    pass
+
+
+def test_getting_convert_makes_request():
+    pass
+
+
+def test_getting_ohlc_makes_request():
+    pass
+
+
+def test_getting_usage_makes_request():
+    pass
