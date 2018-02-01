a82b72d8cb89e243082dadeec481bd91c91bf56d
Alex Hubers <ahubers15@cornellcollege.edu>
2018-02-01 17:03:38 -0600

    Well that was p easy to write

---
+++ boxr/__init__.py
@@ -0,0 +1,78 @@
+import requests
+
+
+def coerce_date(d):
+    '''
+    Coerce whatever `d` is into a string compliant with Open Exchange Rates
+    format (which is as of today yyyy-mm-dd).
+    '''
+    if isinstance(d, str):
+        return d
+    return 'yyyy-mm-dd'
+
+
+class Boxr:
+    base = 'https://openexchangerates.org/api/'
+
+    def __init__(self, app_id):
+        self.app_id = app_id
+        self.session = requests.Session()
+
+    def get(self, url, **kwargs):
+        """
+        Get from the Open Exchange Rates API.
+
+        Function signature matches that of requests.get().
+        """
+        return self.session.get(
+            self.base + url,
+            **{**kwargs, **{'app_id': self.app_id}}
+        )
+
+    def latest(self, **kwargs):
+        '''
+        See https://docs.openexchangerates.org/docs/latest-json
+        '''
+        return self.get('latest.json', **kwargs)
+
+    def historical(self, date, **kwargs):
+        '''
+        See https://docs.openexchangerates.org/docs/historical-json
+        '''
+        return self.get('historical/%s.json' % coerce_date(date), **kwargs)
+
+    def currencies(self, **kwargs):
+        '''
+        See https://docs.openexchangerates.org/docs/currencies-json
+        '''
+        return self.get('currencies.json', **kwargs)
+
+    def time_series(self, **kwargs):
+        '''
+        see https://docs.openexchangerates.org/docs/time-series-json
+        '''
+        self.get('time-series.json', **kwargs)
+
+    def convert(self, value, frum, to, **kwargs):
+        '''
+        See https://docs.openexchangerates.org/docs/convert
+
+        Note: `from` is a reserved keyword and I actually do know
+        how to spell "from".
+        '''
+        return self.get(
+            'convert/%s/%s/%s' % (value, frum, to),
+            **kwargs
+        )
+
+    def ohlc(self, **kwargs):
+        '''
+        See https://docs.openexchangerates.org/docs/ohlc-json
+        '''
+        return self.get('ohlc.json', **kwargs)
+
+    def usage(self, **kwargs):
+        '''
+        See https://docs.openexchangerates.org/docs/usage-json
+        '''
+        return self.get('usage.json', **kwargs)
