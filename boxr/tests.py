4f69b79362cf5a4725e4dd0214f1c3dff84dec82
Alex Hubers <ahubers15@cornellcollege.edu>
2018-02-02 09:33:50 -0600

    Fix tests

--- boxr/tests.py
+++ boxr/tests.py
@@ -1,7 +1,14 @@
 from datetime import date
-from unittest.mock import patch
+from unittest.mock import MagicMock
 
 from . import coerce_date, Boxr
+
+
+def get_test_client_and_session():
+    session = MagicMock()
+    client = Boxr('fake_app_id', session=session)
+    client.base = 'http://noswayzenowayze.com/'
+    return client, session
 
 
 def test_coercing_a_string():
@@ -12,40 +19,52 @@ def test_coercing_a_date():
     assert coerce_date(date(2017, 1, 1)) == '2017-01-01'
 
 
-@patch('__main__.Session')
-def test_get_calls_session_get(session_mock):
-    client = Boxr('fake_app_id')
-    client.base = 'http://noswayzenowayze.com/'
+def test_get_calls_session_get():
+    client, session = get_test_client_and_session()
     client.get('foobar', param1=1, param2=2)
-    session_mock.return_value.get.assert_called_with(
+    session.get.assert_called_with(
         'http://noswayzenowayze.com/foobar',
-        params={'param1': 1, 'param2': 2}
+        params={'param1': 1, 'param2': 2, 'app_id': 'fake_app_id'}
     )
 
 
 def test_getting_latest_makes_request():
-    pass
+    client, session = get_test_client_and_session()
+    client.latest()
+    assert session.get.called
 
 
 def test_getting_historical_makes_request():
-    pass
+    client, session = get_test_client_and_session()
+    client.historical('2017-04-20')
+    assert session.get.called
 
 
 def test_getting_currencies_makes_request():
-    pass
+    client, session = get_test_client_and_session()
+    client.currencies()
+    assert session.get.called
 
 
 def test_getting_time_series_makes_request():
-    pass
+    client, session = get_test_client_and_session()
+    client.time_series()
+    assert session.get.called
 
 
-def test_getting_convert_makes_request():
-    pass
+def test_convert_makes_request():
+    client, session = get_test_client_and_session()
+    client.convert(1, 'USD', 'EUR')
+    assert session.get.called
 
 
 def test_getting_ohlc_makes_request():
-    pass
+    client, session = get_test_client_and_session()
+    client.ohlc()
+    assert session.get.called
 
 
 def test_getting_usage_makes_request():
-    pass
+    client, session = get_test_client_and_session()
+    client.usage()
+    assert session.get.called
